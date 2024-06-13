49e047329328d838f1451f5344b9901efa600e8c
“Charithra” <charithra@adtechnologies.lk>
2024-06-13 10:48:57 +0530

    edit frontend url

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: 'https://food-app-frontend-9vra.vercel.app',
+    // origin: 'https://food-app-frontend-9vra.vercel.app',
+    // credentials: true,
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
 }));
