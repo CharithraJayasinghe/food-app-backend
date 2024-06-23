ea51067d137da1a70c4464a91ee461e2998355f8
“Charithra” <charithra@adtechnologies.lk>
2024-06-23 09:46:43 +0530

    change front end url

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    // origin: 'https://food-app-frontend-9vra.vercel.app',
-    // credentials: true,
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-
-    origin: 'http://localhost:5173',
+    origin: 'https://food-app-frontend-9vra.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+
+    // origin: 'http://localhost:5173',
+    // credentials: true,
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
 }));
 
 app.use('/api/users', userRoutes);
