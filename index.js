5c41c79f80db3f5f368dac9d2934265e3606c141
“Charithra” <charithra@adtechnologies.lk>
2024-07-17 16:44:48 +0530

    path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -29,12 +30,12 @@ app.use(cors({
 
 
 app.use('/api/users', userRoutes);
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, 'public')));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 
